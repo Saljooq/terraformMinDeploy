0df760f772c74ec74759ee15cd5ae6c2bf9dab73
Sal <altaf.saljooq@outlook.com>
2025-02-13 20:50:08 -0600

    added output to get the latest public dns

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,8 @@ output "ec2_public_ip" {
   description = "The public IP address of the EC2 instance"
   value       = aws_instance.webserver.public_ip
 }
+
+output "ec2_public_dns" {
+  description = "The public DNS address of the EC2 instance"
+  value       = aws_instance.webserver.public_dns
+}
